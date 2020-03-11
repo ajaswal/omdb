28e717ce79a5689a36e450d4ecd8fdb5d30aafb1
Ajay Aswal <aswal.ajay@hotmail.com>
2020-03-11 13:34:30 +1100

    reorganise in folder

---
+++ src/components/MovieDetail.js
@@ -0,0 +1,51 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import styled from "styled-components";
+
+export default function MovieDetail() {
+  const { selectedMovie } = useContext(GlobalContext);
+
+  return (
+    <div>
+      {selectedMovie && selectedMovie.Response === "True" && (
+        <StyledMain>
+          <StyledDetails>
+            <h2>{selectedMovie.Title}</h2>
+            <p>{selectedMovie.Genre}</p>
+            <p>Movie plot - {selectedMovie.Plot}</p>
+            <p>
+              <strong>Language:</strong> {selectedMovie.Language}
+            </p>
+            <p>
+              <strong>Director:</strong> {selectedMovie.Director}
+            </p>
+            <p>
+              <strong>Actors:</strong> {selectedMovie.Actors}
+            </p>
+            <p>
+              <strong>Duration:</strong> {selectedMovie.Runtime}
+            </p>
+          </StyledDetails>
+          <StyledPoster>
+            <img src={selectedMovie.Poster} alt={selectedMovie.Title} />
+          </StyledPoster>
+        </StyledMain>
+      )}
+    </div>
+  );
+}
+
+const StyledMain = styled.div`
+  grid-column: auto;
+  grid-row: auto;
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 350px auto;
+`;
+const StyledDetails = styled.div`
+  grid-column: 1;
+`;
+
+const StyledPoster = styled.div`
+  grid-column: 2;
+`;
---
+++ src/components/MovieList.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect, useContext } from "react";
+import styled from "styled-components";
+
+import { GlobalContext } from "../context/GlobalState";
+import Selexted from "./selected.svg";
+
+export default function MovieList() {
+  const [movieId, setMovieId] = useState("1");
+  const [movieDetail, setMovieDetail] = useState({});
+  const [pageNumber, setPageNumber] = useState(1);
+  const { movies, gotMovie } = useContext(GlobalContext);
+ 
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=c4b21b04&i=${movieId}`
+      );
+      const json = await response.json();
+      gotMovie(json)
+      setMovieDetail(json);
+    }
+    fetchData();
+  }, [movieId]);
+  return (
+    <div>
+      {movies.Search &&
+        movies.Search.map((result, index) => (
+          <StyledDiv key={index} onClick={e => setMovieId(result.imdbID)}>
+            <h4>
+              <span>
+                {result.imdbID === movieDetail.imdbID ? "Selected " : ""}
+                {result.Title}
+              </span>
+              {result.imdbID === movieDetail.imdbID && (
+                <img src={Selexted} width={25} alt="selected" />
+              )}
+            </h4>
+            <StyledYear>{result.Year}</StyledYear>
+          </StyledDiv>
+        ))}
+
+      {Number(movies.totalResults) > 10 && (
+        <StyledPagination>
+          <p>
+            <button
+              onClick={e => pageNumber > 1 && setPageNumber(pageNumber - 1)}
+            >
+              &#60;
+            </button>
+            Page
+            {pageNumber}
+            <button onClick={e => setPageNumber(pageNumber + 1)}>&#62;</button>
+          </p>
+          <span>{movies.totalResults || 0} results</span>
+        </StyledPagination>
+      )}
+    </div>
+  );
+}
+const StyledDiv = styled.div`
+  border: 1px solid #000;
+  padding: 10px;
+  margin: 10px;
+`;
+
+const StyledYear = styled.span`
+  display: flex;
+  flex-flow: row-reverse;
+`;
+
+const StyledPagination = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
